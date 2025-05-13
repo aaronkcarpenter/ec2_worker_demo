8fd81ca583924e3cd37457de21dc09d37f59e4e5
Aaron C <aaronkcarpenter@gmail.com>
2025-05-13 18:03:07 -0500

    Private Worker Updated and Working

--- main.tf
+++ main.tf
@@ -9,22 +9,33 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
+  profile = "AWSAdministratorAccess-008971670608"
 }
 
 module "aaron_workerpool" {
   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v4.0.0"
 
-  configuration = <<-EOT
-    export SPACELIFT_TOKEN="${var.worker_pool_config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
-  EOT
+  secure_env_vars = {
+    SPACELIFT_TOKEN            = var.worker_pool_config
+    SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
+  }
 
   min_size                   = 1
   max_size                   = 2
   worker_pool_id             = var.worker_pool_id
   security_groups            = var.worker_pool_security_groups
   vpc_subnets                = var.worker_pool_subnets
-  spacelift_api_key_endpoint = var.spacelift_api_key_endpoint
-  spacelift_api_key_id       = var.spacelift_api_key_id
-  spacelift_api_key_secret   = var.spacelift_api_key_secret
+
+  spacelift_api_credentials = {
+    api_key_id       = var.spacelift_api_key_id
+    api_key_secret   = var.spacelift_api_key_secret
+    api_key_endpoint = var.spacelift_api_key_endpoint
+  }
+
+  autoscaling_configuration = {
+    api_key_id       = var.spacelift_api_key_id
+    api_key_secret   = var.spacelift_api_key_secret
+    api_key_endpoint = var.spacelift_api_key_endpoint
+  }
+
 }
